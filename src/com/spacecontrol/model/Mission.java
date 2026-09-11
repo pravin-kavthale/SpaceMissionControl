6c1aaafa10413370239549f9a114249ec044a411
pravin-kavthale <kavthalepravin@gmail.com>
2026-09-11 22:25:03 +0530

    Add Mission and Spacecraft classes with attributes, constructors, and accessors

--- src/com/spacecontrol/model/Mission.java
+++ src/com/spacecontrol/model/Mission.java
@@ -1,5 +1,94 @@
 package com.spacecontrol.model;
+import java.util.ArrayList;
+import com.spacecontrol.model.Astronaut;
+import com.spacecontrol.enums.*;
 
 public class Mission {
 
+    private int missionId;
+    private String missionName;
+    private String destination;
+    private MissionType missionType;
+	private MissionPriority priority;
+	private MissionStatus status;
+	private int duration;
+
+	private ArrayList<Astronaut> crew;
+	private Spacecraft spacecraft;
+	
+	
+	public String getMissionName() {
+		return missionName;
+	}
+	public void setMissionName(String missionName) {
+		this.missionName = missionName;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public MissionType getMissionType() {
+		return missionType;
+	}
+	public void setMissionType(MissionType missionType) {
+		this.missionType = missionType;
+	}
+	public MissionPriority getPriority() {
+		return priority;
+	}
+	public void setPriority(MissionPriority priority) {
+		this.priority = priority;
+	}
+	public MissionStatus getStatus() {
+		return status;
+	}
+	public void setStatus(MissionStatus status) {
+		this.status = status;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public ArrayList<Astronaut> getCrew() {
+		return crew;
+	}
+	public void setCrew(ArrayList<Astronaut> crew) {
+		this.crew = crew;
+	}
+	public Spacecraft getSpacecraft() {
+		return spacecraft;
+	}
+	public void setSpacecraft(Spacecraft spacecraft) {
+		this.spacecraft = spacecraft;
+	}
+	public int getMissionId() {
+		return missionId;
+	}
+	public Mission(int missionId, String missionName, String destination, MissionType missionType,
+			MissionPriority priority, MissionStatus status, int duration, ArrayList<Astronaut> crew,
+			Spacecraft spacecraft) {
+		super();
+		this.missionId = missionId;
+		this.missionName = missionName;
+		this.destination = destination;
+		this.missionType = missionType;
+		this.priority = priority;
+		this.status = status;
+		this.duration = duration;
+		this.crew = crew;
+		this.spacecraft = spacecraft;
+	}
+	@Override
+	public String toString() {
+		return "Mission [missionId=" + missionId + ", missionName=" + missionName + ", destination=" + destination
+				+ ", missionType=" + missionType + ", priority=" + priority + ", status=" + status + ", duration="
+				+ duration + ", crew=" + crew + ", spacecraft=" + spacecraft + "]";
+	}
+	
+	
+	
 }
--- src/com/spacecontrol/model/Spacecraft.java
+++ src/com/spacecontrol/model/Spacecraft.java
@@ -1,5 +1,68 @@
 package com.spacecontrol.model;
+import com.spacecontrol.enums.SpacecraftStatus;
+
 
 public class Spacecraft {
 
+	private int spacecraftId;
+	private String name;
+	private String type;
+	private int 	capacity;
+	private int 	fuelLevel;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public int getFuelLevel() {
+		return fuelLevel;
+	}
+	public void setFuelLevel(int fuelLevel) {
+		this.fuelLevel = fuelLevel;
+	}
+	public SpacecraftStatus getStatus() {
+		return status;
+	}
+	public void setStatus(SpacecraftStatus status) {
+		this.status = status;
+	}
+	public int getSpacecraftId() {
+		return spacecraftId;
+	}
+	
+	
+	
+	
+	public Spacecraft(int spacecraftId, String name, String type, int capacity, int fuelLevel,
+			SpacecraftStatus status) {
+		super();
+		this.spacecraftId = spacecraftId;
+		this.name = name;
+		this.type = type;
+		this.capacity = capacity;
+		this.fuelLevel = fuelLevel;
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "Spacecraft [spacecraftId=" + spacecraftId + ", name=" + name + ", type=" + type + ", capacity="
+				+ capacity + ", fuelLevel=" + fuelLevel + ", status=" + status + "]";
+	}
+	private SpacecraftStatus status;
 }
